0a16588872351960128fa797b618b31d4cebaf2c
Ezra Singh <ezra@liontech.nyc>
2020-04-11 00:48:51 -0400

    Bootstrapped server entry point

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+
+import Application from "./app";
+
+const PORT = process.env.PORT || 5000;
+
+const app = new Application();
+
+async function startServer(port: number) {
+	try {
+		const connection = await createConnection();
+		app.server.listen(port, () => {
+			// tslint:disable-next-line:no-console
+			console.log(`Server stated on port ${port}`);
+		})
+	} catch(error) {
+		// tslint:disable-next-line:no-console
+		console.log(error);
+	}
+}
+
+startServer(PORT as number);
