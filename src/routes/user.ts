b3bc1904443d16a5c478bdb5e0f8868bab9d5ab7
Ezra Singh <ezra@liontech.nyc>
2020-04-11 00:52:20 -0400

    Bootstrapped API routes

---
+++ src/routes/user.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import UserController from "../controllers/UserController";
+import { checkJwt } from "../middlewares/checkJwt";
+import { checkRole } from "../middlewares/checkRole";
+
+const router = Router();
+
+/** Get all users */
+router.get('/', [checkJwt, checkRole(["ADMIN"])], UserController.listAll);
+
+/** Get one user */
+router.get('/:id([0-9]+)', [checkJwt, checkRole(["ADMIN"])], UserController.getUser);
+
+/** Create new user */
+router.post('/', [checkJwt, checkRole(["ADMIN"])], UserController.createUser);
+
+/** Edit one user */
+router.patch('/:id([0-9]+)', [checkJwt, checkRole(["ADMIN"])], UserController.updateUser);
+
+/** Delete one user */
+router.delete('/:id([0-9]+', [checkJwt, checkRole(["ADMIN"])], UserController.deleteUser);
+
+export default router;
+
