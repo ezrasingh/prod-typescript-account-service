2858a51025f12e9f2ea4ff7ec92248f05948e5f5
Ezra Singh <ezra@liontech.nyc>
2020-04-16 20:57:51 -0400

    utilzed UserRole enum for checkRole middleware

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserController from '../controllers/UserController';
 import { checkJwt } from '../middlewares/checkJwt';
 import { checkRole } from '../middlewares/checkRole';
+import { UserRole } from '../models/User';
 
 const router = Router();
 
@@ -12,29 +13,37 @@ router.get('/me', [checkJwt], UserController.whoami);
 // ? Administration CRUD operations
 
 /** Get all users */
-router.get('/', [checkJwt, checkRole(['ADMIN'])], UserController.listAll);
+router.get(
+	'/',
+	[checkJwt, checkRole([UserRole.ADMIN])],
+	UserController.listAll
+);
 
 /** Get one user */
 router.get(
 	'/:id([0-9]+)',
-	[checkJwt, checkRole(['ADMIN'])],
+	[checkJwt, checkRole([UserRole.ADMIN])],
 	UserController.getUser
 );
 
 /** Create new user */
-router.post('/', [checkJwt, checkRole(['ADMIN'])], UserController.createUser);
+router.post(
+	'/',
+	[checkJwt, checkRole([UserRole.ADMIN])],
+	UserController.createUser
+);
 
 /** Edit one user */
 router.patch(
 	'/:id([0-9]+)',
-	[checkJwt, checkRole(['ADMIN'])],
+	[checkJwt, checkRole([UserRole.ADMIN])],
 	UserController.updateUser
 );
 
 /** Delete one user */
 router.delete(
 	'/:id([0-9]+)',
-	[checkJwt, checkRole(['ADMIN'])],
+	[checkJwt, checkRole([UserRole.ADMIN])],
 	UserController.deleteUser
 );
 
