8254486a818a1421ceb26f8a29dbc96d9ef2aed7
Ezra Singh <ezra@liontech.nyc>
2020-04-20 16:58:55 -0400

    Configuring Postrgres 11 replication

--- docker/db/pgmaster/setup-replication.sh
+++ docker/db/pgmaster/setup-replication.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
-set -e
+
+# ? configure master node
+cat >> ${PGDATA}/postgresql.conf <<EOF
+wal_level = replica           # 9.6+ onwards
+archive_mode = on
+archive_command = 'rsync -av %p $PG_DATA/backups/archive/%f' # shared location somewhere
+archive_timeout = 30 # Forced WAL switch after 30 sec.
+wal_max_Senders = 2 # concurrent connections from standby
+wal_keep_segments = 30 # Keeping last 30 WAL's
+log_connections = on # info. regarding connections or failures.
+EOF
+
+# ? configure pg_hba
+cat > ${PGDATA}/pg_hba.conf <<EOF
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+# "local" is for Unix domain socket connections only
+local   all             all                                     trust
+local   replication      $PG_POOL_USER  all          md5
+EOF
 
 # ? create replication pool user
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE USER $PG_POOL_USER REPLICATION LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '$PG_POOL_PASSWORD';
 EOSQL
-
-# ? configure master node
-cat >> ${PGDATA}/postgresql.conf <<EOF
-wal_level = replica
-archive_mode = on
-archive_command = 'cd .'
-max_wal_senders = 8
-wal_keep_segments = 8
-hot_standby = on
-EOF
--- docker/db/pgreplica/docker-entrypoint.sh
+++ docker/db/pgreplica/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
-echo "*:*:*:$PG_REP_USER:$PG_REP_PASSWORD" > ~/.pgpass
+echo "*:*:*:$PG_POOL_USER:$PG_POOL_PASSWORD" > ~/.pgpass
 
 chmod 0600 ~/.pgpass
 
@@ -11,37 +11,32 @@ until ping -c 1 -W 1 ${PG_MASTER_HOST:?missing environment variable. PG_MASTER_H
     sleep 1s
 done
 
+mkdir -p ${PG_DATA}/backups/archive
+
 # ? synchronize pg master data with pg pool
-until pg_basebackup -h ${PG_MASTER_HOST} -D ${PGDATA} -U ${PG_POOL_USER} --wal-method=fetch
+until pg_basebackup -h ${PG_MASTER_HOST} -D ${PGDATA} -U ${PG_POOL_USER} --wal-method=stream --no-password
   do
     echo "Waiting for master to connect..."
     sleep 1s
 done
 
-# ? set pool connection options in pg_hba.conf
-echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
-
 set -e
 
-# ? set recovery options
-cat > ${PGDATA}/recovery.conf <<EOF
-standby_mode = on
-primary_conninfo = 'host=$PG_MASTER_HOST port=${PG_MASTER_PORT:-5432} user=$PG_POOL_USER password=$PG_POOL_PASSWORD'
-trigger_file = '/tmp/masterNow'
-EOF
-
-chown postgres. ${PGDATA} -R
+chown postgres ${PGDATA} -R
 chmod 700 ${PGDATA} -R
 fi
 
 # ? configure replication node
 cat >> ${PGDATA}/postgresql.conf <<EOF
-wal_level = replica
-# archive_mode = on
-# archive_command = 'cd .'
-max_wal_senders = 8
-wal_keep_segments = 8
+port = 5432
 hot_standby = on
+EOF
+
+# ? set recovery options
+cat > ${PGDATA}/recovery.conf <<EOF
+standby_mode = on
+primary_conninfo = 'host=$PG_MASTER_HOST user=$PG_POOL_USER password=$PG_POOL_PASSWORD'
+archive_cleanup_command = 'pg_archivecleanup $PG_DATA/backups/archive %r'
 EOF
 
 exec "$@"
