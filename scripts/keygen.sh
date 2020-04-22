f65bbbeef4daa03930aa79f851cf978b686d93aa
Ezra Singh <ezra@liontech.nyc>
2020-04-22 09:15:57 -0400

    Simplifed keygen with OpenSSL library

--- scripts/keygen.sh
+++ scripts/keygen.sh
@@ -2,22 +2,6 @@
 
 set -e
 
-function create-cert () {
-  local input_file=$1
-  local output_file=$(echo $2.pem)
+openssl genrsa -out private.pem 4096
 
-  # ? encode key file
-  openssl base64 -in ${input_file} -out ${output_file}
-
-  # ? sanitize the new PEM file
-  cat ${output_file} | tr -d ' ' < ${output_file} | cat /dev/null
-
-  rm ${input_file}
-}
-
-ssh-keygen -q -t rsa -N '' -f key <<< y >/dev/null
-
-create-cert key private
-
-create-cert key.pub public
-
+openssl rsa -in private.pem -outform PEM -pubout -out public.pem
